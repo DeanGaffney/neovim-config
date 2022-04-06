4400744b9e72af7edcac1a2b0a6a2653657a787e
Dean Gaffney <deanpatrickgaffney@gmail.com>
2022-04-06 12:42:23 +0100

    add space key as leader key

--- lua/user-keymaps.lua
+++ lua/user-keymaps.lua
@@ -1,6 +1,9 @@
 -- General keymap settings
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true } )
-
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true } )
+-- Remap space as leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 -- lspsaga, better UI diagnostics for lsp
 vim.api.nvim_set_keymap('n', '<silent>K', ':Lspsaga hover_doc', { noremap = true, silent = true } )
 vim.api.nvim_set_keymap('i', '<C-k>', ':Lspsaga signature_help', { noremap = true, silent = true} )
