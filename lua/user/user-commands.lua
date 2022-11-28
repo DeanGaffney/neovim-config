ce3137c48f76ed3b449fd11f1940fab258808324
Dean Gaffney <deanpatrickgaffney@gmail.com>
2022-11-28 10:37:18 +0100

    feat: updated JiraLink command to remove blank lines

--- lua/user/user-commands.lua
+++ lua/user/user-commands.lua
@@ -1,5 +1,6 @@
 vim.api.nvim_create_user_command(
 	"JiraLink",
-	':read !echo "- https://servisbot.atlassian.next/browse/$(git branch --show-current)"',
+	-- First line is blank, and the output is placed on the second line, so delete the first line
+	":execute 'read !echo \"- https://servisbot.atlassian.next/browse/$(git branch --show-current)\"' | normal ggdd",
 	{}
 )
