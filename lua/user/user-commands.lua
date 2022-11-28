0318c4699cba1a20723a8e29fc0307acc95d6b2b
Dean Gaffney <deanpatrickgaffney@gmail.com>
2022-11-28 11:08:06 +0100

    fix: spelling mistake in jira link

--- lua/user/user-commands.lua
+++ lua/user/user-commands.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_user_command(
 	"JiraLink",
 	-- First line is blank, and the output is placed on the second line, so delete the first line
-	":execute 'read !echo \"- https://servisbot.atlassian.next/browse/$(git branch --show-current)\"' | normal ggdd",
+	":execute 'read !echo \"- https://servisbot.atlassian.net/browse/$(git branch --show-current)\"' | normal ggdd",
 	{}
 )
