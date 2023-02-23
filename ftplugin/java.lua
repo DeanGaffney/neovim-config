13ca4f0bcb6a4cce81941632b40a29d2480b99a7
Dean Gaffney <deanpatrickgaffney@gmail.com>
2023-02-23 10:17:50 +0000

    fix: added auto formatting for java files

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -86,6 +86,17 @@ local on_attach = function(client, bufnr)
 		vim.lsp.buf.format({ async = true })
 	end, bufopts, "Format file")
 
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = "LspFormatting",
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = true })
+			end,
+		})
+	end
+
 	-- Java extensions provided by jdtls
 	nnoremap("<C-o>", jdtls.organize_imports, bufopts, "Organize imports")
 	nnoremap("<space>ev", jdtls.extract_variable, bufopts, "Extract variable")
