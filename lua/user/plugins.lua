99907770adb33d9689abff762698e181e662cb65
Dean Gaffney <deanpatrickgaffney@gmail.com>
2025-11-18 11:43:34 +0000

    fix: removed amazonq plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,227 +2,220 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-	-- Lots of plugins are dependent on this
-	{ "nvim-lua/plenary.nvim" },
+  -- Lots of plugins are dependent on this
+  { "nvim-lua/plenary.nvim" },
 
-	-- Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		opts = {
-			defaults = {
-				wrap_results = true,
-				layout_strategy = "vertical",
-				file_ignore_patterns = {
-					"node_modules",
-					".git",
-					"venv",
-				},
-			},
-		},
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        wrap_results = true,
+        layout_strategy = "vertical",
+        file_ignore_patterns = {
+          "node_modules",
+          ".git",
+          "venv",
+        },
+      },
+    },
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
-	-- LSP
-	{ "williamboman/mason.nvim", lazy = false, build = ":MasonUpdate" },
-	{ "williamboman/mason-lspconfig.nvim", lazy = false },
-	{ "neovim/nvim-lspconfig", lazy = false },
+  -- LSP
+  { "williamboman/mason.nvim",           lazy = false, build = ":MasonUpdate" },
+  { "williamboman/mason-lspconfig.nvim", lazy = false },
+  { "neovim/nvim-lspconfig",             lazy = false },
 
-	-- Lsp status
-	{
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-		config = function()
-			require("fidget").setup()
-		end,
-	},
+  -- Lsp status
+  {
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    config = function()
+      require("fidget").setup()
+    end,
+  },
 
-	-- Autocompletion
-	{ "hrsh7th/nvim-cmp" },
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/cmp-buffer" },
-	{ "hrsh7th/cmp-nvim-lsp-signature-help" },
-	{ "hrsh7th/cmp-nvim-lsp-document-symbol" },
-	{ "hrsh7th/cmp-path" },
-	{ "hrsh7th/cmp-cmdline" },
-	{ "hrsh7th/cmp-vsnip" },
-	{ "hrsh7th/vim-vsnip" },
-	{
-		"L3MON4D3/LuaSnip",
-		build = "make install_jsregexp",
-		dependencies = { "rafamadriz/friendly-snippets" },
-	},
-	{ "saadparwaiz1/cmp_luasnip" },
+  -- Autocompletion
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-nvim-lsp-signature-help" },
+  { "hrsh7th/cmp-nvim-lsp-document-symbol" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "hrsh7th/cmp-vsnip" },
+  { "hrsh7th/vim-vsnip" },
+  {
+    "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
+  { "saadparwaiz1/cmp_luasnip" },
 
-	-- Snippets
-	{ "rafamadriz/friendly-snippets" },
+  -- Snippets
+  { "rafamadriz/friendly-snippets" },
 
-	-- Formatting, Diagnostics, Code Actions
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-	},
-	{
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-	},
+  -- Formatting, Diagnostics, Code Actions
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+  },
 
-	{ "jiangmiao/auto-pairs" },
+  { "jiangmiao/auto-pairs" },
 
-	-- Testing
-	{ "vim-test/vim-test" },
+  -- Testing
+  { "vim-test/vim-test" },
 
-	-- Treesitter
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	-- Additional textobjects for treesitter
-	{ "nvim-treesitter/nvim-treesitter-textobjects" },
-	{ "RRethy/nvim-treesitter-textsubjects" },
-	{ "nvim-treesitter/nvim-treesitter-context" },
+  -- Treesitter
+  { "nvim-treesitter/nvim-treesitter",            build = ":TSUpdate" },
+  -- Additional textobjects for treesitter
+  { "nvim-treesitter/nvim-treesitter-textobjects" },
+  { "RRethy/nvim-treesitter-textsubjects" },
+  { "nvim-treesitter/nvim-treesitter-context" },
 
-	{
-		"yamatsum/nvim-cursorline",
-		config = function()
-			require("nvim-cursorline").setup({
-				cursorline = {
-					enable = true,
-					timeout = 1000,
-					number = false,
-				},
-				cursorword = {
-					enable = true,
-					min_length = 3,
-					hl = { underline = true },
-				},
-			})
-		end,
-	},
+  {
+    "yamatsum/nvim-cursorline",
+    config = function()
+      require("nvim-cursorline").setup({
+        cursorline = {
+          enable = true,
+          timeout = 1000,
+          number = false,
+        },
+        cursorword = {
+          enable = true,
+          min_length = 3,
+          hl = { underline = true },
+        },
+      })
+    end,
+  },
 
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+  },
 
-	-- Icons
-	{ "kyazdani42/nvim-web-devicons", lazy = true },
+  -- Icons
+  { "kyazdani42/nvim-web-devicons", lazy = true },
 
-	-- Status line
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "kyazdani42/nvim-web-devicons" },
-	},
+  -- Status line
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+  },
 
-	-- Comment.nvim
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
+  -- Comment.nvim
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
 
-	-- Git
-	{ "akinsho/git-conflict.nvim", version = "*", config = true },
-	{ "tpope/vim-fugitive" },
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-	},
+  -- Git
+  { "akinsho/git-conflict.nvim",    version = "*", config = true },
+  { "tpope/vim-fugitive" },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
 
-	-- File Explorer
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		-- Optional dependencies
-		dependencies = { "kyazdani42/nvim-web-devicons" },
-	},
+  -- File Explorer
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+  },
 
-	-- Themes
-	{
-		"loctvl842/monokai-pro.nvim",
-		config = function()
-			require("monokai-pro").setup({
-				filter = "classic",
-			})
-			vim.cmd([[colorscheme monokai-pro]])
-		end,
-	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		enabled = false,
-	},
+  -- Themes
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup({
+        filter = "classic",
+      })
+      vim.cmd([[colorscheme monokai-pro]])
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    enabled = false,
+  },
 
-	-- Doc Comments
-	{
-		"danymat/neogen",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
-	},
+  -- Doc Comments
+  {
+    "danymat/neogen",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
 
-	{
-		"folke/trouble.nvim",
-		dependencies = { "kyazdani42/nvim-web-devicons" },
-	},
+  {
+    "folke/trouble.nvim",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+  },
 
-	-- Frequent file navigation
-	{ "ThePrimeagen/harpoon" },
+  -- Frequent file navigation
+  { "ThePrimeagen/harpoon" },
 
-	-- Surround utility
-	{ "tpope/vim-surround" },
+  -- Surround utility
+  { "tpope/vim-surround" },
 
-	-- Zen Mode
-	{ "folke/zen-mode.nvim" },
+  -- Zen Mode
+  { "folke/zen-mode.nvim" },
 
-	-- Enhanced LSP signature help
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "InsertEnter",
-		opts = {},
-		config = function(_, opts) require'lsp_signature'.setup(opts) end
-	},
+  -- Enhanced LSP signature help
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "InsertEnter",
+    opts = {},
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
 
-	-- Find and Replace
-	{
-		"nvim-pack/nvim-spectre",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-
-	-- plugins.lua
-	{
-		name = "amazonq",
-		url = "https://github.com/awslabs/amazonq.nvim.git",
-		opts = {
-			ssoStartUrl = "https://genesys-cloud.awsapps.com/start",
-		},
-	},
+  -- Find and Replace
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 }
 
 require("lazy").setup(plugins)
